7417ebd0a5046ab26ec5255a37d6b8060ec99525
fuyang <815159503@qq.com>
2019-07-20 16:52:08 +0800

    商品

--- src/main/java/com/uniteproject/mapper/GoodsMapper.java
+++ src/main/java/com/uniteproject/mapper/GoodsMapper.java
@@ -1,9 +1,10 @@
 package com.uniteproject.mapper;
 
 import com.uniteproject.pojo.Goods;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-
+@Mapper
 public interface GoodsMapper {
     List<Goods> getGoodsByType(int num, int goodsTypeId,int count);
 
