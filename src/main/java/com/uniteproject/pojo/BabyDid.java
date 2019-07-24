e0b143858d669db2860f2d065a3f1e8e9909479e
daigaokai <1019436676@qq.com>
2019-07-24 15:18:56 +0800

    1516

--- src/main/java/com/uniteproject/pojo/BabyDid.java
+++ src/main/java/com/uniteproject/pojo/BabyDid.java
@@ -1,5 +1,9 @@
 package com.uniteproject.pojo;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class BabyDid {
 
     //宝宝成就表
@@ -8,6 +12,29 @@ public class BabyDid {
     private String didDesc;
     private String didImg;
     private int userId;
+    private Date time;
+
+    //页面取值取这个日期
+    private String newTime;
+
+    public Date getTime() {
+
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getNewTime() {
+        DateFormat sdf = new SimpleDateFormat("yyyy:MM:dd HH:mm:ss");
+        newTime = sdf.format(time);
+        return newTime;
+    }
+
+    public void setNewTime(String newTime) {
+        this.newTime = newTime;
+    }
 
     public int getId() {
         return id;
